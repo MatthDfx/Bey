0de9107d6124ab66f22ab570c0f39fee176a8200
Matthieu Dufloux <matthieu.dufloux@gmail.com>
2023-02-01 22:36:50 +0100

    update admin

--- src/Form/GifType.php
+++ src/Form/GifType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Gif;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,17 @@ class GifType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('url');
+            ->add('url', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'gif.jpg',
+                    'maxlength' => 255,
+                ],
+                'label_attr' => [
+                    'class' => 'm-3',
+                ],
+                'required' => true,
+                'label' => 'Gif',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
